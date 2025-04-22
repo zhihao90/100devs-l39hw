658e2eb90543016f2a0bd1989fd6220fd3b7b73a
Wang Zhihao <zhihao90@gmail.com>
2025-04-22 12:51:12 -0700

    Added Delete and Update Functionalities

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3002
 
+app.use(express.json())
+
 let movies = {
     "21 jump street":{
         'quotes': [`He's white, that means people actually give a shit.`, 
                     `You have the right to... suck my dick, motherfucker!`,
-                    ` Clearly I wasn't talking to you, big-titties.`]
+                    ` Clearly I wasn't talking to you, big-titties.`],
+        'id': '1'
 
     },
     "white chicks":{
@@ -14,19 +17,25 @@ let movies = {
                 ` You hit like a bitch! C'mon.`,
                 `Jesus, lady! All this for just a handbag? It's not *just* a handbag. It's Prada!`,
                 `What a beautiful chocolate man! Beautiful!`
-        ]
+        ],
+        'id': '2'
 
     },
     "zootopia":{
         'quotes':[
             `Life isn't some cartoon musical where you sing a little song and all your insipid dreams magically come true. So let it go.`,
             `I know everybody. And I also know that somewhere there's a toy store missing its stuffed animal. So why don't you get back to your box?`,
-            `You want to talk about making the world a better place, no better way to do it than becoming a carrot farmer.`
-    ]
+            `You want to talk about making the world a better place, no better way to do it than becoming a carrot farmer.`,
+            `I, um... I may have sold him a very expensive wool rug that was made from the fur of a skunk... 's butt.`,
+            `Actually, it's your word against yours. `
+            ],
+
+        'id': '3'
+
     }
 }
 
-let rappers = "NO SUCH MOVIE"
+let noMovieFound = "NO SUCH MOVIE"
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
@@ -36,20 +45,51 @@ app.get('/', (request, response) => {
 app.get('/api/movies', (request, response) => {
     
         response.json(movies)
-        console.log(movies['zootopia']['quotes'][0])
     
 })
 
 app.get('/api/:name', (request, response) => {
     const movieName = request.params.name.toLowerCase()
-    const quoteNum = Math.floor(Math.random()*3)
-    console.log(quoteNum)
-    if(movies[movieName]){
+        if(movies[movieName]){
+        const quoteNum = Math.floor(Math.random()*(movies[movieName]['quotes'].length))
         response.json(movies[movieName]['quotes'][quoteNum])
     }else{
-        response.json(rappers)
+        response.json(noMovieFound)
     }
 })
+
+app.delete('/api/movies/:id', (request, response) => {
+    const id = request.params.id
+    for (let movie in movies){
+
+        if(movies[movie]['id'] == id){
+            delete movies[movie]
+            return response.status(200).json({ message: 'Movie deleted.' });
+        }
+    }
+  
+    response.status(204).end()
+  })
+
+
+  app.post('/api/movies/:id', (request, response) => {
+
+    const id = request.params.id
+    const note = request.body
+
+    console.log(id)
+    console.log(note["quote"])
+
+    for(let movie in movies){
+        if(movies[movie]["id"] == id){
+            movies[movie]["quotes"].push(note["quote"])
+        }
+    }
+
+
+
+    response.json(note)
+  })
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
